58aaf4e5ff4978d031b4aabc4f69c2a797e5b58e
sam ascheim <sam.ascheim@gmail.com>
2024-06-23 13:11:02 -0400

    feat: cors

---
+++ src/main/java/com/longhike/wordle_killer_api/WebConfig.java
@@ -0,0 +1,22 @@
+package com.longhike.wordle_killer_api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(@NonNull CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("http://localhost:3000", "https://wordle-killer-api-c7649f1933cb.herokuapp.com")
+        .allowedMethods("POST")
+        .allowedHeaders(CorsConfiguration.ALL);
+  }
+
+}
